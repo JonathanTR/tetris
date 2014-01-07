8551f7e4368d0cd92d0cf31a0151efe5fd55e0c4
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-01-06 22:09:33 -0700

    Remove driver from controller

--- public/javascripts/controllers/gameController.js
+++ public/javascripts/controllers/gameController.js
@@ -1,14 +1,6 @@
-// driver = function(game){
-//   piece = game.initPiece()
-//   render(game.board, 'tetrisBoard')
-//   game.dropPiece(piece)
-//   render(game.board, 'tetrisBoard')
-// }
-
 window.onload = function(){
   tetris = new TetrisGame()
   if (document.getElementById('tetrisBoard')) {
     render(tetris.board, 'tetrisBoard')
   }
-  driver(tetris)
 }
