e0d3f7329e6bf087511876fa2b3fe3ba1f7fdb52
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-01-10 00:02:10 -0700

    Add nested describes for clarity

--- spec/javascripts/models/game_spec.js
+++ spec/javascripts/models/game_spec.js
@@ -23,68 +23,72 @@ describe("TetrisBoard", function(){
       testPiece = new Piece(TETROMINOES.I);
     })
 
-    it("can activate tiles, given a piece", function(){
-      tetris.activateTilesFor(testPiece);
-      expect(tetris.board[1][3].active).toEqual(true)
-      expect(tetris.board[1][4].active).toEqual(true)
-      expect(tetris.board[1][5].active).toEqual(true)
-      expect(tetris.board[1][6].active).toEqual(true)
+    describe("tile activation", function(){
+      it("can activate tiles, given a piece", function(){
+        tetris.activateTilesFor(testPiece);
+        expect(tetris.board[1][3].active).toEqual(true)
+        expect(tetris.board[1][4].active).toEqual(true)
+        expect(tetris.board[1][5].active).toEqual(true)
+        expect(tetris.board[1][6].active).toEqual(true)
+      })
+
+      it("can deactivate tiles, given a piece", function(){
+        tetris.activateTilesFor(testPiece);
+        tetris.deactivateTilesFor(testPiece);
+        expect(tetris.board[0][4].active).toEqual(false)
+        expect(tetris.board[1][4].active).toEqual(false)
+        expect(tetris.board[2][4].active).toEqual(false)
+        expect(tetris.board[3][4].active).toEqual(false)
+      })
     })
 
-    it("can deactivate tiles, given a piece", function(){
-      tetris.activateTilesFor(testPiece);
-      tetris.deactivateTilesFor(testPiece);
-      expect(tetris.board[0][4].active).toEqual(false)
-      expect(tetris.board[1][4].active).toEqual(false)
-      expect(tetris.board[2][4].active).toEqual(false)
-      expect(tetris.board[3][4].active).toEqual(false)
-    })
+    describe("when a piece bumps into anther, it", function(){
+      it("should freeze a piece if it hits bottom", function(){
+        for(var row = 2; row < tetris.board.length; row++){
+          testPiece.downOne()
+        }
+        tetris.activateTilesFor(testPiece)
+        expect(testPiece.frozen).toBe(true)
+      })
 
-    it("should freeze a piece if it hits bottom", function(){
-      for(var row = 2; row < tetris.board.length; row++){
+      it("should leftFreeze a piece if it hits the left wall", function(){
+        for(var col = 0; col < 3; col++){
+          testPiece.leftOne()
+        }
+        tetris.activateTilesFor(testPiece)
+        expect(testPiece.leftFrozen).toBe(true)
+      })
+
+      it("should rightFreeze a piece if it hits the right wall", function(){
+        for(var col = 0; col < 3; col++){
+          testPiece.rightOne()
+        }
+        tetris.activateTilesFor(testPiece)
+        expect(testPiece.rightFrozen).toBe(true)
+      })
+
+      it("should freeze a piece if it lands on an active tile", function(){
+        tetris.board[3][3].activate()
         testPiece.downOne()
-      }
-      tetris.activateTilesFor(testPiece)
-      expect(testPiece.frozen).toBe(true)
-    })
+        tetris.activateTilesFor(testPiece)
+        expect(testPiece.frozen).toBe(true)
+      })
 
-    it("should leftFreeze a piece if it hits the left wall", function(){
-      for(var col = 0; col < 3; col++){
+      it("should leftFreeze a piece if it touches an active tile on the side", function(){
+        tetris.board[1][2].activate()
+        tetris.activateTilesFor(testPiece)
         testPiece.leftOne()
-      }
-      tetris.activateTilesFor(testPiece)
-      expect(testPiece.leftFrozen).toBe(true)
-    })
+        expect(testPiece.position).toEqual(TETROMINOES.I.positions[0])
+      })
 
-    it("should rightFreeze a piece if it hits the right wall", function(){
-      for(var col = 0; col < 3; col++){
-        testPiece.rightOne()
-      }
-      tetris.activateTilesFor(testPiece)
-      expect(testPiece.rightFrozen).toBe(true)
-    })
-
-    it("should freeze a piece if it lands on an active tile", function(){
-      tetris.board[3][3].activate()
-      testPiece.downOne()
-      tetris.activateTilesFor(testPiece)
-      expect(testPiece.frozen).toBe(true)
-    })
-
-    it("should leftFreeze a piece if it touches an active tile on the side", function(){
-      tetris.board[1][2].activate()
-      tetris.activateTilesFor(testPiece)
-      testPiece.leftOne()
-      expect(testPiece.position).toEqual(TETROMINOES.I.positions[0])
-    })
-
-    it("should not freeze having active tiles as part of its shape", function(){
-      newTPiece = new Piece(TETROMINOES.T)
-      newTPiece.rotate()
-      tetris.activateTilesFor(newTPiece)
-      newTPiece.rotate()
-      tetris.activateTilesFor(newTPiece)
-      expect(newTPiece.frozen).toBe(false)
+      it("should not freeze having active tiles as part of its shape", function(){
+        newTPiece = new Piece(TETROMINOES.T)
+        newTPiece.rotate()
+        tetris.activateTilesFor(newTPiece)
+        newTPiece.rotate()
+        tetris.activateTilesFor(newTPiece)
+        expect(newTPiece.frozen).toBe(false)
+      })
     })
   })
 })
