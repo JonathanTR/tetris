342e5cae90ad430e0bfc0edb9c1ca0a8ef015cf3
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-03-04 13:44:25 -0700

    Wrap render and clearboard functions in GameView object

--- public/javascripts/gameViews.js
+++ public/javascripts/gameViews.js
@@ -1,24 +1,28 @@
-clearBoard = function(handle){
-  document.getElementById(handle).innerHTML = ''
-}
+var GameViews = {
 
-render = function(board, handle){
-  clearBoard(handle)
-  var domBoard = document.getElementById(handle)
-  var modelBoard = board
-  for(var y = 0; y < modelBoard.length; y++){
-    var row = document.createElement('div')
-    row.setAttribute('class', 'row')
-    for(var x = 0; x < modelBoard[y].length; x++){
-      var modeltile = modelBoard[y][x]
-      var tile = document.createElement('div')
-      if(!modeltile.active){
-        tile.setAttribute('class', 'tile')
-      }else{
-        tile.setAttribute('class', 'tile active')
+  clearBoard: function(handle){
+    document.getElementById(handle).innerHTML = ''
+  },
+
+  render: function(board, handle){
+    this.clearBoard(handle)
+    var domBoard = document.getElementById(handle)
+    var modelBoard = board
+    for(var y = 0; y < modelBoard.length; y++){
+      var row = document.createElement('div')
+      row.setAttribute('class', 'row')
+      for(var x = 0; x < modelBoard[y].length; x++){
+        var modeltile = modelBoard[y][x]
+        var tile = document.createElement('div')
+        if(!modeltile.active){
+          tile.setAttribute('class', 'tile')
+        }else{
+          tile.setAttribute('class', 'tile active')
+        }
+        row.appendChild(tile)
       }
-      row.appendChild(tile)
+      domBoard.appendChild(row)
     }
-    domBoard.appendChild(row)
   }
+  
 }
