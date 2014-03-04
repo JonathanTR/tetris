b0fcdd8bac25f1b5b109eb40963273b6ed79ad03
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-03-04 15:19:20 -0700

    Add initialize and addButtonHandlers functions to GameController
    * addButtonHandlers initializes start and pause buttons

--- public/javascripts/gameControllers.js
+++ public/javascripts/gameControllers.js
@@ -6,3 +6,17 @@ var GameController = function(){
 GameController.prototype.loadEmptyBoard = function(){
   GameViews.render(this.board, 'tetrisBoard')
 }
+
+GameController.prototype.runGame = function(){
+  console.log("game started")
+}
+
+GameController.prototype.attachButtonHandlers = function(){
+  var startButton = document.getElementById('start')
+  startButton.addEventListener("click", this.runGame)
+}
+
+GameController.prototype.initialize = function(){
+  this.loadEmptyBoard()
+  this.attachButtonHandlers()
+}
