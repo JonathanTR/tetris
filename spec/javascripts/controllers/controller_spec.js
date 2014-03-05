565e68d5379be281f63ab722607e237c89bd1f25
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-03-04 19:57:38 -0700

    test loadEmptyBoard and attachButtonHandlers in controller

--- spec/javascripts/controllers/controller_spec.js
+++ spec/javascripts/controllers/controller_spec.js
@@ -16,10 +16,33 @@ describe("Game Controller", function(){
 
     it("should load an empty board", function(){
       spyOn(GameViews, "render")
-      tetrisController.loadEmptyBoard();
+      tetrisController.loadEmptyBoard()
       expect(GameViews.render).toHaveBeenCalledWith(board, "tetrisBoard")
     })
 
+  })
+
+  describe("initialize", function(){
+
+    beforeEach(function(){
+      $('body').append("<button id='start'></button>")
+    })
+
+    afterEach(function(){
+      $('#start').remove()
+    })
+
+    it("should run loadEmptyBoard", function(){
+      spyOn(tetrisController, "loadEmptyBoard")
+      tetrisController.initialize()
+      expect(tetrisController.loadEmptyBoard).toHaveBeenCalled
+    })
+
+    it("should attach button handlers", function(){
+      spyOn(tetrisController, "attachButtonHandlers")
+      tetrisController.initialize()
+      expect(tetrisController.attachButtonHandlers).toHaveBeenCalled
+    })
   })
 
 })
